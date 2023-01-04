4ef0831abbc2c67e502125196095a12b81e3df5e
bshar <bodiasharko@gamil.com>
2023-01-04 23:40:38 +0200

    refactor pages/ranges/utils

--- src/pages/ranges/utils.ts
+++ src/pages/ranges/utils.ts
@@ -2,11 +2,16 @@ import { addDays, chooseRandomly, formatDate, range } from '~/utils';
 
 const baseDate = new Date('2022-01-01');
 
-// TODO could we have more strict type here?
-const colors = ['red', 'green', 'blue'];
+enum ColorsEnum {
+  red = 'red',
+  green = 'green',
+  blue = 'blue'
+}
 
-// TODO could we make this range function infer the type, so we don't get any here?
-export const items: Item[] = range(40, (index) => ({
+export type ColorsType = keyof typeof ColorsEnum
+const colors: ColorsType[] = [ColorsEnum.red, ColorsEnum.green, ColorsEnum.blue];
+
+export const rangeItems: Item[] = range(40, (index) => ({
   date: formatDate(addDays(baseDate, index)),
   color: chooseRandomly(colors),
 }));
@@ -17,25 +22,20 @@ export const dataSample = {
   color: 'red',
 };
 
-// TODO could we use inferred items type here?
 export interface Item {
   date: string;
-  // TODO could we use stronger color type here?
-  color: string;
+  color: ColorsType;
 }
 
 export interface Range {
   start: string;
   end: string;
-  // TODO could we use stronger color type here?
-  color: string;
+  color: ColorsType;
 }
 
-
-// TODO could we type this stronger, so autocomplete by key works?
-export const colorToClassName: Record<string, string> = {
-  red: 'bg-red-300 text-red-900',
-  green: 'bg-green-300 text-green-900',
-  blue: 'bg-blue-300 text-blue-900',
-};
+export enum ColorClassNameEnum {
+  red = 'bg-red-300 text-red-900',
+  green = 'bg-green-300 text-green-900',
+  blue = 'bg-blue-300 text-blue-900',
+}
 
